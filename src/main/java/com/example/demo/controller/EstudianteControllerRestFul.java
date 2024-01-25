7249c184de374b08458ca019ff0dbaca809fe989
Juan Jumbo <tiniyan24@gmail.com>
2024-01-24 20:44:12 -0500

    first commit

---
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.modelo.Estudiante;
+//API:Un proyecto API
+
+//Servicio -> Controller: Clase controller
+//Capacidades -> Metodos de una clase
+@RestController//Servicio puede tener muchos servicios
+public class EstudianteControllerRestFul {
+
+	//Capacidades: Metodos
+	public void guardar(Estudiante estudiante) {
+		
+		
+	}
+	
+	
+	
+}
