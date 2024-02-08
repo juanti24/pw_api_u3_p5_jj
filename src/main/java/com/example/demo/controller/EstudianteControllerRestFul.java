f03112d0ad1d0477b77a297d4f4168c6a35ec64a
Juan Jumbo <tiniyan24@gmail.com>
2024-02-07 22:28:39 -0500

    Tarea9: Nivel 2 Modelo de Madurez de Richardson

--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,8 +1,14 @@
 package com.example.demo.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -30,13 +36,21 @@ public class EstudianteControllerRestFul {
 
     //Metodosd = capacidades
 
-    @GetMapping(path="/{id}")  
-    public Estudiante buscar(@PathVariable Integer id){
-        return estudianteService.buscar(id);
+    @GetMapping(path="/{id}", produces = "application/xml")  
+    public ResponseEntity<Estudiante> buscar(@PathVariable Integer id){
+        //240: grupo satisfactorias
+    	//240: Recurso Estudiante encontrado satisfactoriamente
+    	//Contrato de la API (documento pdf o el uso de Swagger.io)
+    	//Esto se implementa en las cabeceras el mensaje o significado de este codigo
+    	
+    	Estudiante estu = this.estudianteService.buscar(id);
+    	
+    	return ResponseEntity.status(HttpStatus.OK).body(estu);
+    	
     }
 
    // http://localhost:8080/API/v1.0/Matricula/estudiantes/buscar
-    @PostMapping
+    @PostMapping()
     public void guardar(@RequestBody Estudiante estudiante){
         this.estudianteService.guardar(estudiante);
     }
@@ -55,11 +69,16 @@ public class EstudianteControllerRestFul {
     }
 
     //filtrar un conjunto/lista los datos
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     // http://localhost:8080/API/v1.0/Matricula/estudiantes/consultarTodos?genero=f&edad=15
-    public List<Estudiante> consultarTodos(@RequestParam (required = false, defaultValue = "M") String genero){
-     
-        return this.estudianteService.buscarTodos(genero);
+    public ResponseEntity<List<Estudiante>>  consultarTodos(@RequestParam (required = false, defaultValue = "M") String genero){
+    	List<Estudiante> lista = this.estudianteService.buscarTodos(genero);
+       
+    	HttpHeaders cabeceras = new HttpHeaders();
+        
+    	cabeceras.add("mensaje_242","Lista consultada de manera satisfactoria");
+    	
+    	return new ResponseEntity<>(lista,cabeceras,242);
     }
 
 
